2d1ad8651db04cec3798cf42acd75ceb33b8e6aa
ax3337 <ax3337@gmail.com>
2018-10-16 17:54:07 +0300

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -2,7 +2,7 @@
 module.exports = {
 	// dashboard UI language
 	language: 'ru',
-	apiBaseUrl: 'http://localhost:3001/api/v1',
-	apiWebSocketUrl: 'ws://localhost:3001',
+	//apiBaseUrl: 'http://localhost:3001/api/v1',
+	//apiWebSocketUrl: 'ws://localhost:3001',
 	developerMode: true
 };
